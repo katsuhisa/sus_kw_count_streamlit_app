a01468329010c3aebb063d3e602bf5c005c8140b
kdeto-uelab <deto.ue.see.eng.osaka-u.ac.jp>
2024-03-26 09:24:10 +0900

    app.pyで利用していないファイル表記を削除

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 # app.py
 import streamlit as st
-from page import upload_csv, classify_1_3, classify_1_2, classify_1, batch_process, batch_process_graph
+from page import batch_process, batch_process_graph
 
 # ページを追加するための辞書
 PAGES = {
     # "分類CSVアップロード": upload_csv,
     "単語出現回数カウント": batch_process,
-    "グラフ描写": batch_process_graph,
-    "分類3まで指定して処理": classify_1_3,
-    "分類2まで指定して処理": classify_1_2,
-    "分類1まで指定して処理": classify_1
+    "グラフ描写": batch_process_graph
 }
 
 def main():
